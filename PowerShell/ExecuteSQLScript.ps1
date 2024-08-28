4bf3b6b06835707d182ca37b39803ce32d024610
Sam Hiscox <its.sam.imba@gmail.com>
2024-08-28 10:11:07 +0100

    Increase invoke-SQL command timeout to 60 seconds

--- PowerShell/ExecuteSQLScript.ps1
+++ PowerShell/ExecuteSQLScript.ps1
@@ -89,7 +89,7 @@ Write-Host "Target database: $sqlDbName"
 
 try {
     # Attempt to execute the SQL script with verbose, and include the message in the standard output for error handling
-    $output = Invoke-Sqlcmd -ServerInstance $sqlServerFQDN -Database $sqlDbName -Username $sqlServerAdminUserName -Password $sqlServerPassword -Query $sqlScript -OutputSqlErrors $true -IncludeSqlUserErrors -ErrorAction Stop -Verbose 4>&1
+    $output = Invoke-Sqlcmd -ServerInstance $sqlServerFQDN -Database $sqlDbName -Username $sqlServerAdminUserName -Password $sqlServerPassword -Query $sqlScript -OutputSqlErrors $true -IncludeSqlUserErrors -ErrorAction Stop -Verbose 4>&1 -Querytimeout 90
 
     Write-Host "SQL script executed successfully: $output"
 
